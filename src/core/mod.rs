407edebe4da8aa3e83520976042cac3e2ef65e76
fuzhou.chen <fuzhouch@gmail.com>
2016-10-26 18:44:21 +0800

    [New] Add module structure.

---
+++ src/core/mod.rs
@@ -0,0 +1,6 @@
+// vim:ft=rust expandtab tw=72 sw=4
+// copyright: https://github.com/maiosite/rustruct/blob/master/LICENSE
+
+mod doctree;
+
+pub use self::doctree::DocumentTree;
---
+++ src/input/mod.rs
@@ -0,0 +1,6 @@
+// vim:ft=rust expandtab tw=72 sw=4
+// copyright: https://github.com/maiosite/rustruct/blob/master/LICENSE
+
+mod rstparser;
+
+pub use self::rstparser::{ReStructuredText};
