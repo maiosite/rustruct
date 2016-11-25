eb0e141b8df613b915a90ee25844ba27754ab2da
fuzhou.chen <fuzhouch@gmail.com>
2016-11-26 00:23:47 +0800

    [New] Reconstruct folder structure.
    This is the first `final' definition structure of model definitions.
    It will be used during the first release.

--- src/core/mod.rs
+++
@@ -1,6 +0,0 @@
-// vim:ft=rust expandtab tw=72 sw=4
-// copyright: https://github.com/maiosite/rustruct/blob/master/LICENSE
-
-mod doctree;
-
-pub use self::doctree::DocumentTree;
--- src/output/mod.rs
+++
@@ -1,6 +0,0 @@
-// vim:ft=rust expandtab tw=72 sw=4
-// copyright: https://github.com/maiosite/rustruct/blob/master/LICENSE
-
-mod html5;
-
-pub use self::html5::Html5Writer;
