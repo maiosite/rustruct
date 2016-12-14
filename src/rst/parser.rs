1b46d61a6f577222e2478cd1fda955e23d80c68a
fuzhou.chen <fuzhouch@gmail.com>
2016-12-15 01:16:56 +0800

    [New] Remove build warnings by calling recognize() in public functions.

--- src/rst/parser.rs
+++ src/rst/parser.rs
@@ -2,6 +2,7 @@
 // Licensed under MIT 2.0 license. See LICENSE file for details.
 
 use super::error::Error;
+use super::doclines::recognize;
 use super::super::doctree::elements::Document;
 
 /// Main interface to geneate doctree from ReStructuredText document.
@@ -17,6 +18,11 @@ impl ReStructuredText {
 
     /// Read ReStructuredText documentation and return an doctree.
     pub fn read_string(content: &String) -> Result<Document, Error> {
+        let lines: Vec<_> = content.lines().collect();
+        // TODO parsing logic goes here
+        for line_no in 0..lines.len() {
+            recognize(&lines, line_no);
+        }
         Ok(Document {})
     }
 }
